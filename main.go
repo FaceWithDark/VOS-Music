9ad7396167619734615b0535cfcde9a44b8370a0
FaceWithDark <114546939+FaceWithDark@users.noreply.github.com>
2025-11-07 13:35:33 +1100

    feat: add more use cases and a greeting message
    I also put it in a nonstop loop as well so user can interact with the
    program continuously.

--- main.go
+++ main.go
@@ -7,52 +7,82 @@ import (
     "strings"
 )
 
+func appIntro() {
+	const greet = "Welcome to VOS Music. Please select an option below to continue\n"
+
+	var options = []string{
+		"1. Add new music",
+		"2. Search for music",
+		"3. Exit the program\n",
+	}
+
+	fmt.Println(greet)
+
+	for _, option := range options {
+		fmt.Println(option)
+	}
+}
+
 func main() {
-    // fmt.Println("TUI Music Library, made in Go!");
-
-    musicComposerInput := bufio.NewReader(os.Stdin)
-    fmt.Print("Enter music composer: ")
-	musicComposer, composerError := musicComposerInput.ReadString('\n')
-	if composerError != nil {
-		fmt.Println("Error reading composer input:", composerError)
-
-		// exit for current condition, not the program because that is how Go
-		// works with early return codes.
-		return
-	} else {
-		/*
-         * Go (and languages support multi-declare variables) will have pretty
-		 * funny errors when nesting methods so it's better to re-assign value
-		 * to existen variable.
-         */
-
-		// Trim <Enter> input (Go treated as '\n')
-        musicComposer = strings.TrimSpace(musicComposer)
-    }
-
-    musicTitleInput := bufio.NewReader(os.Stdin)
-    fmt.Print("Enter music title: ")
-	musicTitle, titleError := musicTitleInput.ReadString('\n')
-    
-	if titleError != nil {
-		fmt.Println("Error reading title input:", titleError)
-
-		// exit for current condition, not the program because that is how Go
-		// works with early return codes.
-		return
-	} else {
-		/*
-         * Go (and languages support multi-declare variables) will have pretty
-		 * funny errors when nesting methods so it's better to re-assign value
-		 * to existen variable.
-         */
-
-		// Trim <Enter> input (Go treated as '\n')
-        musicTitle = strings.TrimSpace(musicTitle)
-    }
+	appIntro()
 
 	var musicLibrary = make(map[string]string)
-	musicLibrary[musicComposer] = musicTitle
+	for {
+		fmt.Print("Enter your option here: ")
+		var userOption int 
+		_ , userError := fmt.Scan(&userOption)
+		if (userError != nil) {
+			fmt.Println("Error reading user input:", userError)
+		} else {
+			switch userOption {
+				case 1:
+					musicComposerInput := bufio.NewReader(os.Stdin)
+					fmt.Print("Enter music composer: ")
+					musicComposer, composerError := musicComposerInput.ReadString('\n')
+					if composerError != nil {
+						fmt.Println("Error reading composer input:", composerError)
+						return
+					} else {
+						/*
+						 * Go (and languages support multi-declare variables) will
+						 * have pretty funny errors when nesting methods so it's
+						 * better to re-assign value to existent variable.
+						 */
 
-	fmt.Println(musicLibrary)
+						// Trim <Enter> input (Go treated as '\n')
+						musicComposer = strings.TrimSpace(musicComposer)
+					}
+
+					musicTitleInput := bufio.NewReader(os.Stdin)
+					fmt.Print("Enter music title: ")
+					musicTitle, titleError := musicTitleInput.ReadString('\n')
+							
+					if titleError != nil {
+						fmt.Println("Error reading title input:", titleError)
+						return
+					} else {
+						/*
+						 * Go (and languages support multi-declare variables) will
+						 * have pretty funny errors when nesting methods so it's
+						 * better to re-assign value to existent variable.
+						 */
+
+						// Trim <Enter> input (Go treated as '\n')
+						musicTitle = strings.TrimSpace(musicTitle)
+					}
+
+					musicLibrary[musicComposer] = musicTitle
+					fmt.Println(musicLibrary)
+
+				case 2:
+					fmt.Println("Coming soon!")
+
+				case 3:
+					fmt.Println("Coming soon!")
+
+				default:
+					fmt.Println("Please try again!")
+			}
+		}
+	}
 }
